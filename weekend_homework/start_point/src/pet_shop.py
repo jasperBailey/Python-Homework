1eef863f2aee8d1f448f660271c025a2ec2755b1
user <user@users-mbp.mynet>
2023-03-03 15:46:56 +0000

    added weekend_homework

---
+++ weekend_homework/start_point/src/pet_shop.py
@@ -0,0 +1,60 @@
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash_change):
+    pet_shop["admin"]["total_cash"] += cash_change
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, num_new_sales):
+    pet_shop["admin"]["pets_sold"] += num_new_sales
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets.append(pet)
+    return pets
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+        
+def remove_pet_by_name(pet_shop, pet_name):
+    pet_shop["pets"].remove(find_pet_by_name(pet_shop, pet_name))
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+def customer_can_afford_pet(customer, new_pet):
+    if get_customer_cash(customer) >= new_pet["price"]:
+        return True
+    return False
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet and customer_can_afford_pet(customer, pet):
+        add_pet_to_customer(customer, pet)
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        remove_pet_by_name(pet_shop, pet["name"])
+        increase_pets_sold(pet_shop, 1)
